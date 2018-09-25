98790ef601561ff34cb8d3562242c3c08cc7192b
Pablo Crivella <pablocrivella@gmail.com>
2018-09-25 09:04:47 +0200

    Refactor specs

--- spec/jsonapi/record/parser_spec.rb
+++ spec/jsonapi/record/parser_spec.rb
@@ -6,24 +6,25 @@ RSpec.describe JSONAPI::Record::Parser do
   subject { described_class.parse(document) }
 
   describe ".parse" do
+    let(:included_comment1) { { id: "1", type: "comments", attributes: { body: "Hi!" } } }
+    let(:included_comment2) { { id: "2", type: "comments", attributes: { body: "Hello!" } } }
+    let(:included) { [included_comment1, included_comment2] }
+    let(:comment1) { included_comment1.slice(:id, :type) }
+    let(:comment2) { included_comment2.slice(:id, :type) }
+    let(:comments_relationship) { { data: [comment1, comment2] } }
+    let(:relationships) { { comments: comments_relationship } }
+    let(:parsed_comment1) { { id: "1", body: "Hi!", persisted: true } }
+    let(:parsed_comment2) { { id: "2", body: "Hello!", persisted: true } }
+
     context "when document is a JSONAPI::Types::Info" do
       let(:document) { JSONAPI::Types::Info.new(meta: { author: "John Doe" }) }
 
       it { is_expected.to eq(top_level_meta: { author: "John Doe" }) }
     end
 
-    context "when document is a JSONAPI::Types::Success" do
-      let(:included_comment1) { { id: "1", type: "comments", attributes: { body: "Hi!" } } }
-      let(:included_comment2) { { id: "2", type: "comments", attributes: { body: "Hello!" } } }
-      let(:included) { [included_comment1, included_comment2] }
-      let(:document) { JSONAPI::Types::Success.new(data: data, included: included) }
+    context "when document is a JSONAPI::Types::Success containing a single resource" do
+      let(:document) { JSONAPI::Types::Success.new(data: user1, included: included) }
       let(:user1) { { id: "1", type: "users", relationships: relationships } }
-      let(:comment1) { included_comment1.slice(:id, :type) }
-      let(:comment2) { included_comment2.slice(:id, :type) }
-      let(:comments_relationship) { { data: [comment1, comment2] } }
-      let(:relationships) { { comments: comments_relationship } }
-      let(:parsed_comment1) { { id: "1", body: "Hi!", persisted: true } }
-      let(:parsed_comment2) { { id: "2", body: "Hello!", persisted: true } }
       let(:parsed_user1) do
         {
           id: "1",
@@ -35,19 +36,15 @@ RSpec.describe JSONAPI::Record::Parser do
         }
       end
 
-      context "when document is a resource" do
-        let(:data) { user1 }
+      it { is_expected.to eq(parsed_user1) }
+    end
 
-        it { is_expected.to eq(parsed_user1) }
-      end
+    context "when document is a JSONAPI::Types::Success containing multiple resources" do
+      let(:document) { JSONAPI::Types::Success.new(data: [user2], included: included) }
+      let(:parsed_user2) { { id: "2", persisted: true } }
+      let(:user2) { { id: "2", type: "users" } }
 
-      context "when document data is an array" do
-        let(:parsed_user2) { { id: "2", persisted: true } }
-        let(:user2) { { id: "2", type: "users" } }
-        let(:data) { [user1, user2] }
-
-        it { is_expected.to eq([parsed_user1, parsed_user2]) }
-      end
+      it { is_expected.to eq([parsed_user2]) }
     end
 
     context "when document is a JSONAPI::Types::Failure" do
--- spec/jsonapi/record/queryable_spec.rb
+++ spec/jsonapi/record/queryable_spec.rb
@@ -3,41 +3,39 @@
 require "spec_helper"
 
 RSpec.describe JSONAPI::Record::Queryable do
-  let(:user) { User.new(id: "1", email: "user@example.com") }
+  let(:user) { User.new(id: "1", email: "user@example.com", persisted: true) }
 
   describe ".all" do
-    let(:fetched_users) { User.all }
-    let!(:get_request_sub) do
+    subject(:fetched_users) { User.all }
+
+    before do
       stub_request(:get, User.collection_uri)
         .to_return(status: 200, body: { data: [user.data] }.to_json)
     end
 
-    it "performs a get request to fetch users" do
-      expect(fetched_users).to be_an Array
-      expect(fetched_users.first.id).to eq user.id
-      expect(fetched_users.first.email).to eq user.email
-      expect(fetched_users.first.persisted?).to eq true
+    it { is_expected.to be_an(Array) }
+
+    it "fetches all users" do
+      expect(fetched_users).to include(user)
     end
   end
 
   describe ".find!" do
-    let(:fetched_user) { User.find!(user.id) }
+    subject(:fetched_user) { User.find!(user.id) }
 
     context "when response status is 200" do
-      let!(:get_request_sub) do
+      before do
         stub_request(:get, User.individual_uri(user.id))
           .to_return(status: 200, body: user.to_payload.to_json)
       end
 
-      it "performs a get request to fetch a specific user" do
-        expect(fetched_user.id).to eq user.id
-        expect(fetched_user.email).to eq user.email
-        expect(fetched_user.persisted?).to eq true
+      it "fetches a specific user" do
+        expect(fetched_user).to eq(user)
       end
     end
 
     context "when response status is 404" do
-      let!(:get_request_sub) { stub_request(:get, User.individual_uri(user.id)).to_return(status: 404) }
+      before { stub_request(:get, User.individual_uri(user.id)).to_return(status: 404) }
 
       it "raises a JSONAPI::Client::NotFound error" do
         expect { fetched_user }.to raise_error JSONAPI::Client::NotFound
@@ -46,14 +44,12 @@ RSpec.describe JSONAPI::Record::Queryable do
   end
 
   describe ".find" do
-    let(:fetched_user) { User.find(user.id) }
+    subject(:fetched_user) { User.find(user.id) }
 
     context "when response status is 404" do
-      let!(:get_request_sub) { stub_request(:get, User.individual_uri(user.id)).to_return(status: 404) }
+      before { stub_request(:get, User.individual_uri(user.id)).to_return(status: 404) }
 
-      it "returns nil" do
-        expect(fetched_user).to be nil
-      end
+      it { is_expected.to be_nil }
     end
   end
 end
